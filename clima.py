27318bf75acad3167c4ab29657da122bbbc1cf3b
Brahim Benmouna <163146496+bnnbra@users.noreply.github.com>
2026-02-21 01:58:54 +0100

    Update clima.py
     Añadida función Kelvin. Closes #1

--- clima.py
+++ clima.py
@@ -3,3 +3,7 @@ def celsius_a_fahrenheit(c):
     return (c * 9/5) + 32
 
 print(f"20°C son {celsius_a_fahrenheit(20)}°F")
+def celsius_a_kelvin(c):
+    return c + 273.15
+
+print(f"20°C son {celsius_a_kelvin(20)}K")
